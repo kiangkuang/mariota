ea8a1488b28ca9a717c6101dc920e1c6d4086992
Kiang Kuang <kiangkuang@gmail.com>
2018-06-06 01:25:29 +0800

    Change API

--- build/js/script.js
+++ build/js/script.js
@@ -2,14 +2,14 @@ function loadJoke(){
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-           document.getElementsByClassName("humour__joke")[0].innerHTML = xhr.responseText;
+            var joke = JSON.parse(xhr.responseText);
+            document.getElementsByClassName("humour__joke")[0].innerHTML = joke.setup + " " + joke.punchline;
+            document.getElementsByClassName("humour__tag")[0].innerHTML = joke.type;
         }
     };
 
-    xhr.open("GET", "https://icanhazdadjoke.com/");
-    xhr.setRequestHeader("Accept", "text/plain");
-
-    xhr.send();   
+    xhr.open("GET", "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke");
+    xhr.send();
 }
 
 loadJoke();
