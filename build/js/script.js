11b068bc8f855e6d3e1ed2186b4606585d2f188f
Kiang Kuang <kiangkuang@gmail.com>
2018-06-08 01:09:12 +0800

    Retry if repeated joke

--- build/js/script.js
+++ build/js/script.js
@@ -1,14 +1,20 @@
 // API: Joke Loading
+var jokes = [];
+loadJoke();
+
 function loadJoke() {
     $.get('https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke')
     .done(function(joke) {
-        $('.humour__joke').text(joke.setup + ' ' + joke.punchline);
-        $('.humour__tag').text(joke.type);
-        $('.splash').addClass('hide');
+        if (!jokes.includes(joke.id)) {
+            jokes.push(joke.id);
+            $('.humour__joke').text(joke.setup + ' ' + joke.punchline);
+            $('.humour__tag').text(joke.type);
+            $('.splash').addClass('hide');
+        } else {
+            loadJoke();
+        }
     });
 }
-
-loadJoke();
 
 
 // CSS Animation: Reload button
