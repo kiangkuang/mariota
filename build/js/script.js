c5b3e76967f2a8679aad67ac47df905c9f11af0f
Kiang Kuang <kiangkuang@gmail.com>
2018-06-08 01:31:48 +0800

    Handle refresh spam

--- build/js/script.js
+++ build/js/script.js
@@ -1,26 +1,24 @@
 // API: Joke Loading
+var lock = false
 var jokes = [];
 loadJoke();
 
-function loadJoke() {
-    $.get('https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke')
-    .done(function(joke) {
-        if (!jokes.includes(joke.id)) {
-            jokes.push(joke.id);
-            $('.humour__joke').text(joke.setup + ' ' + joke.punchline);
-            $('.humour__tag').text(joke.type);
-            $('.splash').addClass('hide');
-        } else {
-            loadJoke();
-        }
-    });
+function loadJoke(retry) {
+    if (!lock || retry) {
+        lock = true;
+        $('.humour__reload').addClass('animate');
+        $.get('https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke')
+        .done(function(joke) {
+            if (!jokes.includes(joke.id)) {
+                jokes.push(joke.id);
+                $('.humour__joke').text(joke.setup + ' ' + joke.punchline);
+                $('.humour__tag').text(joke.type);
+                $('.splash').addClass('hide');
+                $('.humour__reload').removeClass('animate');
+                lock = false;
+            } else {
+                loadJoke(true);
+            }
+        });    
+    }
 }
-
-
-// CSS Animation: Reload button
-$('.humour__reload').click(function() {
-	var $this = $(this).addClass('animate');
-	window.setTimeout(function(){
-		$this.removeClass('animate');
-	}, 1000);
-});
